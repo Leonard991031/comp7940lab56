706544e2ee13fe6ce068bca3be87488f10e2927d
Leonard991031 <xhy1031@yeah.net>
2022-02-23 10:55:48 +0800

    Heroku Chatbot Second Commit

--- chatbot.py
+++ chatbot.py
@@ -17,7 +17,7 @@ def main():
     dispatcher = updater.dispatcher
 
     global redis1
-    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))    
 
     # You can set this logging module, so you will know when and why things do not work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
